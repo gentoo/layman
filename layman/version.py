e9279ed990f591d662c914a7d57c0c56b3c02679
Sebastian Pipping <sebastian@pipping.org>
2009-12-04 21:38:15 +0100

    Prepare release 1.2.4 (sync change log and release notes, bump version)

--- layman/version.py
+++ layman/version.py
@@ -8,17 +8,19 @@
 #             Current version number
 #
 # Copyright:
-#             (c) 2005 - 2008 Gunnar Wrobel
+#             (c) 2005 - 2009 Gunnar Wrobel
+#             (c) 2009        Sebastian Pipping
 #             Distributed under the terms of the GNU General Public License v2
 #
 # Author(s):
 #             Gunnar Wrobel <wrobel@gentoo.org>
+#             Sebastian Pipping <sebastian@pipping.org>
 #
 
 __version__ = "$Id: version.py 309 2007-04-09 16:23:38Z wrobel $"
 
 
-VERSION = '1.2.3'
+VERSION = '1.2.4'
 
 if __name__ == '__main__':
     print VERSION
