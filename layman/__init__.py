b2b7b5899a6f61a6545a5b91c5fe8691f4d29cbe
dol-sen <brian.dolbec@gmail.com>
2011-08-21 00:32:42 -0700

    continue establishing a proper high level interface.

--- layman/__init__.py
+++ layman/__init__.py
@@ -5,10 +5,18 @@
 on all gentoo repositories and overlays
 """
 
+try:
+    from layman.api import LaymanAPI
+    from layman.config import BareConfig
+    from layman.output import Message
+except ImportError:
+    import sys
+    sys.stderr.write("!!! Layman API import failed.")
 
 
-class Layman(object):
-    """High level interface capable of performing all
+
+class Layman(LaymanAPI):
+    """A complete high level interface capable of performing all
     overlay repository actions."""
 
     def __init__(self, stdout=None, stdin=None, stderr=None,
@@ -18,30 +26,22 @@ class Layman(object):
         """Input parameters are optional to override the defaults.
         sets up our LaymanAPI with defaults or passed in values
         and returns an instance of it"""
-        import sys
-        try:
-            from layman.api import LaymanAPI
-            from layman.config import BareConfig
-            from layman.output import Message
-        except ImportError:
-            sys.stderr.write("!!! Layman API import failed.")
-            return None
-        self.message = Message()
+        self.message = Message(out=stdout, err=stderr)
         self.config = BareConfig(
-                output=message,
-                stdout=None,
-                stdin=None,
-                stderr=None,
-                config=None,
-                read_configfile=True,
-                quiet=False,
-                quietness=4,
-                verbose=False,
-                nocolor=False,
-                width=0
+                output=self.message,
+                stdout=stdout,
+                stdin=stdin,
+                stderr=stderr,
+                config=config,
+                read_configfile=read_configfile,
+                quiet=quiet,
+                quietness=quietness,
+                verbose=verbose,
+                nocolor=nocolor,
+                width=width
             )
-        self.layman = LaymanAPI(self.config,
+        LaymanAPI.__init__(self, self.config,
                              report_errors=True,
-                             output=config['output']
+                             output=self.config['output']
                             )
-        return _layman
+        return
