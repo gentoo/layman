f3843493af328c2a220877c61b1e98027dc0e569
dol-sen <brian.dolbec@gmail.com>
2011-10-08 13:25:31 -0700

    spread out the test stages separation.

--- c-layman/run-test.sh
+++ c-layman/run-test.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 echo Compile lib :
 make
+echo 
+echo libs compiled and linked
 
+echo
 echo Compile tester:
 gcc src/tester.c -llayman -g -W -Wall -Isrc/ -L.libs/ --std=c99 -o tester
 
+echo
 echo Run :
 LD_LIBRARY_PATH=.libs/ ./tester
