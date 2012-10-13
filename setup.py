e13bca9defe7d7dce41757cf414c4ad8ac5a7e8c
Brian Dolbec <dolsen@gentoo.org>
2012-10-13 11:40:56 -0700

    Pre-release updates to CHANGES, README, stup.py, etc..

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ from layman.version import VERSION
 setup(name          = 'layman',
       version       = VERSION,
       description   = 'Python script for retrieving gentoo overlays',
-      author        = 'Gunnar Wrobel, Brian Dolbec',
-      author_email  = 'wrobel@gentoo.org, brian.dolbec@gmail.com',
+      author        = 'Brian Dolbec, Gunnar Wrobel (original author retired)',
+      author_email  = 'dolsen@gentoo',
       url           = 'http://layman.sourceforge.net/, ' +\
         'http://git.overlays.gentoo.org/gitweb/?p=proj/layman.git;a=summary',
       packages      = ['layman', 'layman.overlays'],
-      scripts       = ['bin/layman'],
+      scripts       = ['bin/layman', 'bin/layman-updater'],
       license       = 'GPL',
       )
