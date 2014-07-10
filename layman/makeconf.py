4f49d0562e679439f68c7f252adac96b5982097c
Devan Franchini <twitch153@gentoo.org>
2014-07-10 15:59:41 -0400

    makeconf.py: Adds disable and enable functionality
    To allow users to momentarily allow portage to "forget" an overlay
    dir, the ability to disable and then re-enable an overlay has been
    added.
    
    X-Gentoo-Bug: 512316
    X-Gentoo-Bug-URL: https://bugs.gentoo.org/512316

--- layman/makeconf.py
+++ layman/makeconf.py
@@ -72,7 +72,9 @@ class ConfigHandler:
     >>> shutil.rmtree(tmpdir)
     '''
 
-    my_re = re.compile('PORTDIR_OVERLAY\s*=\s*"([^"]*)"')
+    my_enabled_re  = re.compile('ENABLED\s*=\s*"([^"]*)"')
+    my_disabled_re = re.compile('DISABLED\s*=\s*"([^"]*)"')
+    my_portdir_re  = re.compile('PORTDIR_OVERLAY\s*=\s*"([^"]*)"')
 
     def __init__(self, config, overlays):
 
@@ -82,10 +84,12 @@ class ConfigHandler:
         self.data = ''
         self.db = overlays
         self.overlays = []
+        self.disabled = []
         self.extra = []
         self.output = config['output']
 
         self.read(True)
+
 
     def add(self, overlay):
         '''
@@ -118,6 +122,7 @@ class ConfigHandler:
         '''
         self.overlays.append(overlay)
         return self.write()
+
 
     def delete(self, overlay):
         '''
@@ -153,11 +158,33 @@ class ConfigHandler:
                          if i.name != overlay.name]
         return self.write()
 
+
+    def disable(self, overlay):
+        '''
+        Move overlay to the $DISABLED var of make.conf.
+
+        @params overlay: layman.overlay.Overlay object.
+        @rtype bool: represents success or failure to write to make.conf.
+        '''
+        return self.write(disable=overlay.name)
+
+
+    def enable(self, overlay):
+        '''
+        Move overlay to the $ENABLED var of make.conf.
+
+        @params overlay: layman.overlay.Overlay object.
+        @rtype bool: represents success or failure to write to make.conf.
+        '''
+        return self.write(enable=overlay.name)
+
+
     def update(self, overlay):
         '''
         Stub function necessary for RepoConfManager class.
         '''
         pass
+
 
     def read(self, raise_error=False):
         '''
@@ -180,7 +207,9 @@ class ConfigHandler:
         if os.path.isfile(self.path):
             self.content()
 
-            overlays = self.my_re.search(self.data)
+            overlays = self.my_portdir_re.search(self.data)
+            enabled_overlays = self.my_enabled_re.search(self.data)
+            disabled_overlays = self.my_disabled_re.search(self.data)
 
             if not overlays:
                 msg = 'MakeConf: ConfigHandler.read(); Did not find a '\
@@ -195,30 +224,48 @@ class ConfigHandler:
                         for i in overlays.group(1).split('\n')
                         if i.strip()]
 
-            for i in overlays:
-                if i[:len(self.storage)] == self.storage:
-                    oname = os.path.basename(i)
-                    if  oname in self.db.keys():
-                        self.overlays.append(self.db[oname])
+            if enabled_overlays:
+                enabled_overlays = [i.strip()
+                                    for i in enabled_overlays.group(1).split('\n')
+                                    if i.strip()]
+
+            if disabled_overlays:
+                disabled_overlays = [i.strip()
+                                     for i in disabled_overlays.group(1).split('\n')
+                                     if i.strip()]
+
+            for i in (disabled_overlays, enabled_overlays, overlays):
+                for o in i:
+                    if o[:len(self.storage)] == self.storage:
+                        oname = os.path.basename(o)
+                        if  oname in self.db.keys():
+                            if i = disabled_overlays:
+                                self.disabled.append(path([self.storage, oname]))
+                            self.overlays.append(self.db[oname])
+                        else:
+                            # These are additional overlays that we dont know
+                            # anything about. The user probably added them manually
+                            self.extra.append(o)
                     else:
-                        # These are additional overlays that we dont know
-                        # anything about. The user probably added them manually
-                        self.extra.append(i)
-                else:
-                    # These are additional overlays that we dont know anything
-                    # about. The user probably added them manually
-                    self.extra.append(i)
+                        # These are additional overlays that we dont know anything
+                        # about. The user probably added them manually
+                        self.extra.append(o)
 
         else:
             self.overlays = []
             self.data     = 'PORTDIR_OVERLAY="\n"\n'
 
         self.extra = [i for i in self.extra
-                         if (i != '$PORTDIR_OVERLAY'
-                             and i != '${PORTDIR_OVERLAY}')]
+                         if ((i != '$PORTDIR_OVERLAY'
+                             and i != '${PORTDIR_OVERLAY}')
+                             and (i != '$ENABLED'
+                             and i != '${ENABLED}')
+                             and (i != '$DISABLED'
+                             and i != '${DISABLED}'))]
         return True
 
-    def write(self):
+
+    def write(self, disable=None, enable=None):
         '''
         Write the list of registered overlays to /var/layman/make.conf.
 
@@ -257,17 +304,51 @@ class ConfigHandler:
 
         self.overlays.sort(key=cmp_to_key(prio_sort))
 
-        paths = []
-        for i in self.overlays:
-            paths.append(path((self.storage, i.name, )))
+        enabled = []
 
+        for i in self.overlays:
+            ovl = path([self.storage, i.name])
+            # We want to enable this overlay:
+            if i.name == enable:
+                if ovl in self.disabled:
+                    self.disabled.remove(ovl)
+                    enabled.append(ovl)
+                else:
+                    msg = 'Overlay "%(repo)s" is already enabled!'\
+                            % ({'repo': i.name})
+                    self.output.error(msg)
+                    return False
+            # We want to disable this overlay:
+            elif i.name == disable:
+                if ovl not in self.disabled:
+                    self.disabled.append(ovl)
+                else:
+                    msg = 'Overlay "%(repo)s" is already disabled!'\
+                            % ({'repo': i.name})
+                    self.output.error(msg)
+            # We want to keep it as-is:
+            else:
+                if ovl not in self.disabled:
+                    enabled.append(ovl)
+
+        self.disabled.sort()
+
+
+        enabled_overlays = 'ENABLED="\n'
+        enabled_overlays += '\n'.join(enabled) + '\n' if enabled else ''
+        enabled_overlays += '"'
+        disabled_overlays = 'DISABLED="\n'
+        disabled_overlays += '\n'.join(self.disabled) + '\n' if self.disabled else ''
+        disabled_overlays += '"'
         overlays = 'PORTDIR_OVERLAY="\n'
-        overlays += '\n'.join(paths) + '\n'
+        overlays += '$ENABLED\n'
         overlays += '$PORTDIR_OVERLAY\n'
         overlays += '\n'.join(self.extra)
         overlays += '"'
 
-        content = self.my_re.sub(overlays, self.data)
+        enabled_content = self.my_enabled_re.sub(enabled_overlays, self.data)
+        disabled_content = self.my_disabled_re.sub(disabled_overlays, enabled_content)
+        content = self.my_portdir_re.sub(overlays, disabled_content)
 
         if not self.my_re.search(content):
             self.output.error('MakeConf: ConfigHandler.write(); Oops, failed to set a '\
