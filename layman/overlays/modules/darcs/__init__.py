2eb6cd7d517a14a8b80537fd1a8ca8b8ab28fe14
Devan Franchini <twitch153@gentoo.org>
2014-07-28 20:44:30 -0400

    darcs.py: Converts to plug-in module

---
+++ layman/overlays/modules/darcs/__init__.py
@@ -0,0 +1,25 @@
+# Copyright 2014 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+'''
+Darcs plug-in module for layman.
+'''
+
+module_spec = {
+    'name': 'darcs',
+    'description': __doc__,
+    'provides':{
+        'darcs-module': {
+            'name': 'darcs',
+            'class': 'DarcsOverlay',
+            'description': __doc__,
+            'functions': ['add', 'supported', 'sync'],
+            'func_desc': {
+                'add': 'Performs a darcs get on a repository',
+                'supported': 'Confirms if overlay type is supported',
+                'sync': 'Performs a darcs pull on the repository',
+            },
+        }
+    }
+}
+
