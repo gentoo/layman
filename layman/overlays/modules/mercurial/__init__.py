754084ab0925bbc924b691119d7ebada0f92f048
Devan Franchini <twitch153@gentoo.org>
2014-07-28 21:44:50 -0400

    mercurial.py: Converts to plug-in module

---
+++ layman/overlays/modules/mercurial/__init__.py
@@ -0,0 +1,26 @@
+# Copyright 2014 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+'''
+Mercurial plug-in module for layman.
+'''
+
+module_spec = {
+    'name': 'mercurial',
+    'description': __doc__,
+    'provides':{
+        'mercurial-module': {
+            'name': 'mercurial',
+            'class': 'MercurialOverlay',
+            'description': __doc__,
+            'functions': ['add', 'supported', 'sync', 'update'],
+            'func_desc': {
+                'add': 'Performs a hg clone on a repository',
+                'supported': 'Confirms if overlay type is supported',
+                'sync': 'Performs a hg pull on the repository',
+                'update': 'Updates a mercurial overlay\'s source URL',
+            },
+        }
+    }
+}
+
