23a73e67ab6a9be9ce9c8f5a4b814bc84b8fbca9
Devan Franchini <twitch153@gentoo.org>
2014-07-28 20:40:24 -0400

    cvs.py: Converts to plug-in module

---
+++ layman/overlays/modules/cvs/__init__.py
@@ -0,0 +1,26 @@
+# Copyright 2014 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+'''
+CVS plug-in module for layman.
+'''
+
+module_spec = {
+    'name': 'cvs',
+    'description': __doc__,
+    'provides':{
+        'cvs-module': {
+            'name': 'cvs',
+            'class': 'CvsOverlay',
+            'description': __doc__,
+            'functions': ['add', 'supported', 'sync', 'update'],
+            'func_desc': {
+                'add': 'Performs a cvs checkout on a repository',
+                'supported': 'Confirms if overlay type is supported',
+                'sync': 'Performs a cvs update on the repository',
+                'update': 'Updates a cvs overlay\'s source URL',
+            },
+        }
+    }
+}
+
